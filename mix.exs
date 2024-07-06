9d396b0578baa22bcc6bd9393e78f546baee5132
Lars Jensen <11350491+lcjtrifork@users.noreply.github.com>
2024-07-06 22:15:41 +0200

    Check unused deps

--- mix.exs
+++ mix.exs
@@ -61,7 +61,7 @@ defmodule Kanban.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
     ]
   end
 
